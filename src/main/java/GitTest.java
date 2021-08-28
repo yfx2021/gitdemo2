c0dbb10d1c2459540da8edf88d0353e6767672fc
yifan <miracleyifan@163.com>
2021-08-28 21:59:10 +0800

    master 1st commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -4,5 +4,7 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("master-test");
+
     }
 }

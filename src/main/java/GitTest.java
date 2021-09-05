73a0761e68777431d34b17f92ddc3cfeaff572ba
yifan <miracleyifan@163.com>
2021-09-05 17:15:38 +0800

    gitee2

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -8,6 +8,6 @@ public class GitTest {
         System.out.println("hot-fix");
         System.out.println("push test");
         System.out.println("pull test");
-        
+        System.out.println("pull test");
     }
 }
